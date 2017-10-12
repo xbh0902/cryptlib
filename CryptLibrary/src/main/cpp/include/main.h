a7d948cbf4c7859b42a2ed9764100f780d0fb13c
wenbin.zhou <wenbin.zhou@ehking.com>
2017-10-12 18:09:46 +0800

    随机16位aes密钥生成

--- CryptLibrary/src/main/cpp/include/main.h
+++ CryptLibrary/src/main/cpp/include/main.h
@@ -1,23 +1,21 @@
 #ifndef CRYPTLIB_MAIN_H
 #define CRYPTLIB_MAIN_H
+
 #include "xlib.h"
 #include <jni.h>
+extern "C" {
 
-extern "C"
-JNIEXPORT jstring JNICALL
-Java_me_xbh_lib_impl_AesImpl_getRandomKey(JNIEnv *env);
+    JNIEXPORT jstring JNICALL
+    Java_me_xbh_lib_impl_AesImpl_getRandomKey(JNIEnv *env);
 
-extern "C"
-JNIEXPORT jstring JNICALL
-Java_me_xbh_lib_Md5_digest(JNIEnv *env, jclass clazz, jstring plainText);
+    JNIEXPORT jstring JNICALL
+    Java_me_xbh_lib_Md5_digest(JNIEnv *env, jclass clazz, jstring plainText);
 
+    JNIEXPORT jstring JNICALL
+    Java_me_xbh_lib_impl_AesImpl_encrypt(JNIEnv *env, jclass clazz, jstring plainText, jstring key);
 
-extern "C"
-JNIEXPORT jstring JNICALL
-Java_me_xbh_lib_impl_AesImpl_encrypt(JNIEnv *env, jclass clazz, jstring plainText, jstring key);
+    JNIEXPORT jstring JNICALL
+    Java_me_xbh_lib_impl_AesImpl_decrypt(JNIEnv *env, jclass clazz, jstring cipherText, jstring key);
 
-extern "C"
-JNIEXPORT jstring JNICALL
-Java_me_xbh_lib_impl_AesImpl_decrypt(JNIEnv *env, jclass clazz, jstring cipherText, jstring key);
+}
 #endif //CRYPTLIB_MAIN_H
-
