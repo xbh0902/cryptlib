559fc5f97a777bd320c4c8f510ad1a8e4f4c9201
wenbin.zhou <wenbin.zhou@ehking.com>
2017-10-22 10:06:59 +0800

    '解决冲突'

--- CryptLibrary/src/main/cpp/main.cpp
+++ CryptLibrary/src/main/cpp/main.cpp
@@ -3,7 +3,6 @@
 #include "md5.h"
 #include "random_tool.h"
 #include "aes.h"
-#include <exception>
 
 JNIEXPORT jstring JNICALL
 Java_me_xbh_lib_Md5_digest(JNIEnv *env, jclass clazz, jstring plainText) {
@@ -26,6 +25,7 @@ Java_me_xbh_lib_impl_AesImpl_encode(JNIEnv *env, jclass clazz, jstring plainText
     return env->NewStringUTF(result.c_str());
 }
 
+JNIEXPORT jstring JNICALL
 Java_me_xbh_lib_impl_AesImpl_decode(JNIEnv *env, jclass clazz, jstring cipherText, jstring key) {
 
     AES *aes = NULL;
